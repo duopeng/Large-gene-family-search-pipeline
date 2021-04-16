6b8ad5d3d5ee9636f20b23d9ce6306b2c17802e9
Duo Peng <pengxunduo@gmail.com>
2021-04-15 22:12:11 -0400

    readme

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Pipeline that search for members of large gene families in Pacbio/Nanopore genomes
 
+## Prerequisites
+Linux-like environment
+Two executable programs "blastn" and "makeblastdb" from the ncbi-blast suite (tested with version 2.8.1+)
+Python3 (tested with version 3.7.0)
+PERL (tested with v5.18.2)
+Bioperl (tested with v1.006924)
 
 ## Getting Started
-To be expanded
+The scripts folder contains scripts and input files for a working example, in which the the T cruzi genome is searched for mucin gene family members
 
 ##### Folder Structure:
 ```
-
 .
-├── scripts          # scripts used to do family search
+├── scripts             # scripts used to do family search
+│     │
+│     └──input_files    #input files for a working example
 │
 └── README.md
-
--- scripts/1.blast.cmd.sh
+++ scripts/1.blast.cmd.sh
@@ -1,5 +1,8 @@
 #############################################
-#change reverse blast gene name filter!!
+#Parameters:
+#
+BLASTN_path = "/home/duopeng/bin/x86_64/ncbi-blast-2.8.1+/bin/blastn"
+#
 #############################################
 
 genomedir=/home/duopeng/gene_member_search/genomes
@@ -22,16 +25,16 @@ if [ ${all_or_full} == "full" ]; then
 	genefamilyfasta=${all_and_full}/${familyname}/Full_ClBrener_${familyname}_DNA_sequence.fasta
 fi
 if [ ${all_or_full} == "all" ]; then
-	genefamilyfasta=${all_and_full}/${familyname}/All_ClBrener_${familyname}_DNA_sequence.fasta	
-fi	
+	genefamilyfasta=${all_and_full}/${familyname}/All_ClBrener_${familyname}_DNA_sequence.fasta
+fi
 if [ ${all_or_full} == "WW" ]; then
-	genefamilyfasta=${all_and_full}/${familyname}/WW_${genome_name}_${familyname}_DNA_sequence.fasta	
-fi	
+	genefamilyfasta=${all_and_full}/${familyname}/WW_${genome_name}_${familyname}_DNA_sequence.fasta
+fi
 if [ ${all_or_full} == "40ts" ]; then
-	genefamilyfasta=${all_and_full}/${familyname}/40ts_${familyname}_DNA_sequence.fasta	
-fi	
+	genefamilyfasta=${all_and_full}/${familyname}/40ts_${familyname}_DNA_sequence.fasta
+fi
 
-genomefasta=${genome_name}.fasta  
+genomefasta=${genome_name}.fasta
 
 
 genefamilydir=/home/duopeng/gene_member_search/family_search_${familyname}_${genome_name}_${all_or_full}
@@ -39,7 +42,7 @@ codedir=${genefamilydir}
 cd ${genefamilydir}
 
 #blast
-/home/duopeng/bin/x86_64/ncbi-blast-2.8.1+/bin/blastn -db ${genomedir}/${genomefasta} -query ${genefamilyfasta} -out ${perc_identity_cutoff}percid.2.8.1.blastout -num_threads 1 -num_alignments 100 -max_hsps 100 -perc_identity ${perc_identity_cutoff}
+${BLASTN_path} -db ${genomedir}/${genomefasta} -query ${genefamilyfasta} -out ${perc_identity_cutoff}percid.2.8.1.blastout -num_threads 1 -num_alignments 100 -max_hsps 100 -perc_identity ${perc_identity_cutoff}
 
 #parse blast, get gene candidates
 perl ${codedir}/mark_homology_pieces.pl -i ${perc_identity_cutoff}percid.2.8.1.blastout -g ${genomedir}/${genomefasta} -maxhspgap ${mapHspGap} -minTcTSLength ${minTcTSLength} -gff ${genefamilygff} -familyfasta ${genefamilyfasta}  > log_of_gene_num.txt
@@ -47,7 +50,7 @@ rm ${perc_identity_cutoff}percid.2.8.1.blastout
 
 
 #blast back to all transcripts
-/home/duopeng/bin/x86_64/ncbi-blast-2.8.1+/bin/blastn -outfmt 5 -db /home/duopeng/TcTS_transcripts/TriTrypDB-34_TcruziCLBrener_AnnotatedTranscripts.fasta -query ${genefamilydir}/${perc_identity_cutoff}percid.2.8.1.blastout.${genomefasta}.mintctslengthcutoff${minTcTSLength}.maxgaplenintcts${mapHspGap}.fasta -out ${perc_identity_cutoff}perc_${minTcTSLength}minlen_${mapHspGap}maxgap.blast2transcripts.blastoutxml -num_threads 1 -num_alignments 50 -max_hsps 50
+${BLASTN_path} -outfmt 5 -db /home/duopeng/TcTS_transcripts/TriTrypDB-34_TcruziCLBrener_AnnotatedTranscripts.fasta -query ${genefamilydir}/${perc_identity_cutoff}percid.2.8.1.blastout.${genomefasta}.mintctslengthcutoff${minTcTSLength}.maxgaplenintcts${mapHspGap}.fasta -out ${perc_identity_cutoff}perc_${minTcTSLength}minlen_${mapHspGap}maxgap.blast2transcripts.blastoutxml -num_threads 1 -num_alignments 50 -max_hsps 50
 
 #parse blast
 python3 parse_blastout.edited.py --blastout ${perc_identity_cutoff}perc_${minTcTSLength}minlen_${mapHspGap}maxgap.blast2transcripts.blastoutxml --fasta ${genefamilydir}/${perc_identity_cutoff}percid.2.8.1.blastout.${genomefasta}.mintctslengthcutoff${minTcTSLength}.maxgaplenintcts${mapHspGap}.fasta
@@ -58,4 +61,3 @@ python3 adjust_start_stop_codon.py --fastafile 85perc_150minlen_0maxgap.blast2tr
 
 #get length distribution
 python3 Get_seq_lengths_from_fasta.v1.1.py --file 85perc_150minlen_0maxgap.blast2transcripts.blastoutxml.parsedNfiltered.adjusted.fasta
-
